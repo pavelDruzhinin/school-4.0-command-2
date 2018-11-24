8511cadd46c9c9422ebcec7c289d241af6bb965f
Alexei Osipov <alxwtf@gmail.com>
2018-11-24 16:18:16 +0300

    FirstCommit

---
+++ SmartTrash/SmartTrash/Models/Dump.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartTrash.Models
+{
+    public class Dump
+    {
+        int id { get; set; }
+        float longitude { get; set; }
+        float latitude { get; set; }
+        string Name { get; set; }
+        decimal Amount { get; set; }
+        decimal Filled { get; set; }
+        decimal PercentOfFill => (100 * Filled)/Amount;
+
+    }
+}
---
+++ SmartTrash/SmartTrash/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SmartTrash.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
