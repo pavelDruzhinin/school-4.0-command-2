31f1aade67a0a3c80a31331c269259e7a0fc46b4
Viktor Ohotnikov <viktorokh96@mail.ru>
2018-11-26 01:42:17 +0300

    Add: Simle fake data generator program

---
+++ SmartTrash/DataGenerator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Configuration.Json;
+using System.IO;
+using System.Collections.Generic;
+using SmartTrash.Data;
+using SmartTrash.Models;
+
+namespace DataGenerator
+{ 
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json")
+                .Build();
+
+            var optionsBuilder = new DbContextOptionsBuilder<TrashContext>()
+                .UseSqlServer(config.GetConnectionString("TrashContext"));
+
+            using (var context = new TrashContext(optionsBuilder.Options))
+            {
+                context.Add(new WasteCollectionArea()
+                    {
+                        Name = "Мусорная площадка 1",
+                        Longitude = 61.790867F,
+                        Latitude = 34.367756F,
+                        Volume = 1100,
+                        FilledVolume = 136
+                    });
+
+                context.SaveChanges();
+            }
+        }
+    }
+}
