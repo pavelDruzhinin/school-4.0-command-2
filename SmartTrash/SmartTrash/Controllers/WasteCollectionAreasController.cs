0250de29a7798e998d7a8e9e16ee23d4571c3164
Alexei Osipov <alxwtf@gmail.com>
2018-11-28 22:57:54 +0300

    Rebuild CRUD

--- SmartTrash/SmartTrash/Controllers/WasteCollectionAreasController.cs
+++ SmartTrash/SmartTrash/Controllers/WasteCollectionAreasController.cs
@@ -26,5 +26,50 @@ namespace SmartTrash.Controllers
 
             return Json(areas);
         }
+
+        [Route("admin")]
+        public IActionResult admin()
+        {
+            var model = _db.WasteCollectionAreas.ToList();
+            return View(model);
+        }
+
+        [Route("admin/add")]
+        [Route("admin/edit/{id:int}")]
+        public async Task<IActionResult> TrashPoints(int? id)
+        {
+            if (!id.HasValue)
+                return View();
+
+            var area = await _db.WasteCollectionAreas.FirstOrDefaultAsync(x => x.Id == id);
+            return View(area);
+        }
+
+        [HttpPost]
+        [Route("Save")]
+        public async Task<IActionResult> Save(WasteCollectionArea area)
+        {
+            if (area.Id == default(int))
+                _db.WasteCollectionAreas.Add(area);
+            else
+            {
+                _db.WasteCollectionAreas.Attach(area);
+                _db.Entry(area).State = EntityState.Modified;
+            }
+            await _db.SaveChangesAsync();
+            return RedirectToAction("admin");
+        }
+
+        [Route("admin/del/{id:int}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            var area = await _db.WasteCollectionAreas.FirstOrDefaultAsync(x => x.Id == id);
+            if (area != null)
+            {
+                _db.WasteCollectionAreas.Remove(area);
+                await _db.SaveChangesAsync();
+            }
+            return RedirectToAction("admin");
+        }
     }
 }
