6f6f76cd62d900d80b856c6a17894ae6eb54ce3d
Andrey Dengin <andreid.86@gmail.com>
2018-11-29 07:07:20 +0300

    add inentity context

---
+++ SmartTrash/SmartTrash/Data/Mappings/UserRoleMap.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SmartTrash.Models.IdentityModels;
+
+namespace SmartTrash.Data.Mappings
+{
+    public class UserRoleMap : IEntityTypeConfiguration<UserRole>
+    {
+        public void Configure(EntityTypeBuilder<UserRole> builder)
+        {
+            builder.HasKey(ur => new { ur.UserId, ur.RoleId });
+
+            builder.HasOne(ur => ur.Role)
+                .WithMany(r => r.UserRoles)
+                .HasForeignKey(ur => ur.RoleId)
+                .IsRequired();
+
+            builder.HasOne(ur => ur.User)
+                .WithMany(u => u.UserRoles)
+                .HasForeignKey(ur => ur.UserId)
+                .IsRequired();
+        }
+    }
+}
--- SmartTrash/SmartTrash/Data/TrashContext.cs
+++ SmartTrash/SmartTrash/Data/TrashContext.cs
@@ -2,13 +2,18 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using SmartTrash.Data.Mappings;
 using SmartTrash.Models;
+using SmartTrash.Models.IdentityModels;
 
 namespace SmartTrash.Data
 {
-    public class TrashContext : DbContext
+    public class TrashContext : IdentityDbContext<User, Role, int,
+        IdentityUserClaim<int>, UserRole, IdentityUserLogin<int>,
+        IdentityRoleClaim<int>, IdentityUserToken<int>>
     {
         public TrashContext(DbContextOptions options) : base(options)
         {
@@ -22,6 +27,7 @@ namespace SmartTrash.Data
             base.OnModelCreating(modelBuilder);
 
             modelBuilder.ApplyConfiguration(new WasteCollectionAreaMap());
+            modelBuilder.ApplyConfiguration(new UserRoleMap());
         }
     }
 }
