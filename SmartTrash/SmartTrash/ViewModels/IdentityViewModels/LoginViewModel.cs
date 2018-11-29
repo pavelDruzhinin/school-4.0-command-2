eaf35ea8aad70fff20c965e3e5c0e35ac3899879
Andrey Dengin <andreid.86@gmail.com>
2018-11-29 07:09:10 +0300

    add identity viewmodels

---
+++ SmartTrash/SmartTrash/ViewModels/IdentityViewModels/LoginViewModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SmartTrash.ViewModels.IdentityViewModels
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [Display(Name = "UserName")]
+        [StringLength(16)]
+        public string UserName { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
+        public string Password { get; set; }
+
+        [Display(Name = "Remember me?")]
+        public bool RememberMe { get; set; } = true;
+    }
+}
