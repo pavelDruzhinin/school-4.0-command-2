b25f53d8fce96e0b827625e0d017d69f652dbd76
Andrey Dengin <andreid.86@gmail.com>
2018-11-29 07:06:42 +0300

    add base identity models

---
+++ SmartTrash/SmartTrash/Models/IdentityModels/Role.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SmartTrash.Models.IdentityModels
+{
+    public class Role : IdentityRole<int>
+    {
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+}
---
+++ SmartTrash/SmartTrash/Models/IdentityModels/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+
+namespace SmartTrash.Models.IdentityModels
+{
+    public class User : IdentityUser<int>
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public ICollection<UserRole> UserRoles { get; set; }
+    }
+}
