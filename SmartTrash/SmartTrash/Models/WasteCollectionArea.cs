4a51e6516d8514d85ffa8baa5202454c2c3a73dd
Alexei Osipov <alxwtf@gmail.com>
2018-11-30 11:37:43 +0300

    fix of add/edit page, add  question mark on latitude, longitude and volume for validation

--- SmartTrash/SmartTrash/Models/WasteCollectionArea.cs
+++ SmartTrash/SmartTrash/Models/WasteCollectionArea.cs
@@ -9,13 +9,32 @@ namespace SmartTrash.Models
     public class WasteCollectionArea
     {
         public int Id { get; set; }
+        [Display(Name = "Название мусорки")]
+        [Required(ErrorMessage = "Введите название точки")]
+        [StringLength(200, MinimumLength = 3, ErrorMessage = "Название должно содержать от 3 до 200 знаков")]
         public string Name { get; set; }
-        public float Latitude { get; set; }
-        public float Longitude { get; set; }
-        public decimal Volume { get; set; }
+        [Display(Name = "Широта")]
+        [Required(ErrorMessage = "Введите широту")]
+        public float? Latitude { get; set; }
+        [Display(Name = "Долгота")]
+        [Required(ErrorMessage = "Введите долготу")]
+        public float? Longitude { get; set; }
+        [Required(ErrorMessage = "Введите объём")]
+        [Display(Name = "Объём")]
+        public decimal? Volume { get; set; }
         public decimal FilledVolume { get; set; }
-        public decimal PercentOfFill=>(100 * FilledVolume) / Volume;
+        public decimal PercentOfFill
+        {
+            get
+            {
+                return Volume == null || Volume==0 ? 0 : (100 * FilledVolume) / (decimal)Volume;
+            }
+        }
 
+        public bool Empty
+        {
+            get { return Id == 0; }
+        }
         public WasteCollectionArea()
         {
 
