5dd5b0bcddcbc06a8c19cb830a8c299a2c1cf540
Andrey Dengin <andreid.86@gmail.com>
2018-12-02 20:29:33 +0300

    add filters partial view

--- SmartTrash/SmartTrash/Controllers/HomeController.cs
+++ SmartTrash/SmartTrash/Controllers/HomeController.cs
@@ -39,5 +39,10 @@ namespace SmartTrash.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
+        public IActionResult GetFilters()
+        {
+            return PartialView("_Filters");
+        }
     }
 }
