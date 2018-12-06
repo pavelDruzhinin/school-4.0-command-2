77c97a86ecb9b2cec4682a6f8695d2efe4548472
Kasmin <victor.kasmin@gmail.com>
2018-12-06 12:25:33 +0300

    Add display geopoints on map on first load page

--- SmartTrash/SmartTrash/wwwroot/js/map.js
+++ SmartTrash/SmartTrash/wwwroot/js/map.js
@@ -10,4 +10,37 @@ function init() {
         searchControlProvider: 'yandex#search'
     });
 
+    var geoPoints = new Array;
+
+    points.forEach(function (elem) {
+        myPieChart = new ymaps.Placemark([
+            elem.latitude, elem.longitude // dynamic
+        ], {
+                // Данные для построения диаграммы.
+                data: [
+                    { weight: (100 - elem.percentOfFill), color: '#f5f5f5' }, // dynamic
+                    { weight: elem.percentOfFill, color: 'red' }, // dynamic
+                ],
+                balloonContent: elem.name, // dynamic
+                iconContent: Math.round(elem.percentOfFill) // dynamic
+            }, {
+                // Зададим произвольный макет метки.
+                iconLayout: 'default#pieChart',
+                // Радиус диаграммы в пикселях.
+                iconPieChartRadius: 25,
+                // Радиус центральной части макета.
+                iconPieChartCoreRadius: 15,
+                // Стиль заливки центральной части.
+                iconPieChartCoreFillStyle: '#ffffff',
+                // Cтиль линий-разделителей секторов и внешней обводки диаграммы.
+                iconPieChartStrokeStyle: '#ffffff',
+                // Ширина линий-разделителей секторов и внешней обводки диаграммы.
+                iconPieChartStrokeWidth: 3,
+                // Максимальная ширина подписи метки.
+                iconPieChartCaptionMaxWidth: 200
+            });
+        // Создаем геообъект.
+        myMap.geoObjects
+            .add(myPieChart);
+    })
 }
