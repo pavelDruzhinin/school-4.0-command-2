3cbc0095954dc80ce646034793e639a750f456ad
Kasmin <victor.kasmin@gmail.com>
2018-12-09 20:25:27 +0300

    Add create route with sorted pins

--- SmartTrash/SmartTrash/wwwroot/js/map.js
+++ SmartTrash/SmartTrash/wwwroot/js/map.js
@@ -1,4 +1,9 @@
 ﻿var myMap;
+// Координаты Автопарка мусоровозов и Полигона ТБО
+var cordBaseStation = [61.757534, 34.417469];
+var cordWasteLandfill = [61.699688, 34.453655];
+// Массив меток для построения маршрута
+var pinsArray = new Array;
 
 ymaps.ready(init);
 
@@ -15,6 +20,8 @@ function init() {
 
 function addGeoPoints(_points) {
     removeGeoPoints();
+    pinsArray.length = 0;
+
     _points.forEach(function (elem) {
         var colorPie;
         var colorPieEmpty = '#f5f5f5';
@@ -57,12 +64,71 @@ function addGeoPoints(_points) {
                 // Максимальная ширина подписи метки.
                 iconPieChartCaptionMaxWidth: 200
             });
-        // Создаем геообъект.
+
+        // Добавляем метку в массив для дальнейшей сортировки
+        pinsArray.push(myPieChart);
+        // Добавляем метку на карту.
         myMap.geoObjects
             .add(myPieChart);
     })
+
+    // Создаем метку для Автопарка мусоровозов
+    var pinBaseStation = new ymaps.Placemark(
+        cordBaseStation,
+        {
+            balloonContent: 'Автопарк мусоровозов'
+        },
+        {
+            preset: 'islands#blueDeliveryIcon',
+            iconColor: 'blue'
+        }
+    );
+    // Создаем метку для Полигонга ТБО
+    var pinWasteLandfill = new ymaps.Placemark(
+        cordWasteLandfill,
+        {
+            balloonContent: 'Полигон ТБО'
+        },
+        {
+            preset: 'islands#redWasteIcon',
+            iconColor: 'red'
+        }
+    );
+    // Добавляем эти 2 метки на карту
+    myMap.geoObjects
+        .add(pinBaseStation)
+        .add(pinWasteLandfill);
 }
 
 function removeGeoPoints() {
     myMap.geoObjects.removeAll();
+}
+
+function createRoute() {
+    // Сортируем метки относительно Автопарка мусоровозов
+    var pinsGeoQuery = ymaps.geoQuery(pinsArray);
+    var pinsSorted = pinsGeoQuery.sortByDistance(cordBaseStation);
+    // Формируем массив координат из отсортированных меток
+    var arraySortCords = new Array;
+    pinsSorted._objects.forEach(function (elem) {
+        arraySortCords.push(elem.geometry._coordinates);
+    });
+    // Добавляем начальную, предполседнюю и последнюю точку для построения итогового маршрута
+    arraySortCords.unshift(cordBaseStation);
+    arraySortCords.push(cordWasteLandfill);
+    arraySortCords.push(cordBaseStation);
+    // Строим маршрут
+    ymaps.route(arraySortCords, {
+        mapStateAutoApply: true
+    }).then(function (route) {
+        route.getPaths().options.set({
+            // в балуне выводим только информацию о времени движения с учетом пробок
+            balloonContentBodyLayout: ymaps.templateLayoutFactory.createClass('$[properties.humanJamsTime]'),
+            // можно выставить настройки графики маршруту
+            strokeColor: '0000ffff',
+            opacity: 0.9
+        });
+        // добавляем маршрут на карту
+        myMap.geoObjects.add(route);
+    });
 }
